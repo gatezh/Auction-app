43837687a634c4dfe9e2e136837767f54d21d78d
Sergii Gatezh <gatezh@users.noreply.github.com>
2017-03-31 10:45:58 -0400

    Add components and product service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,19 +2,41 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { CarouselComponent } from './carousel/carousel.component';
+import { FooterComponent } from './footer/footer.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { ProductItemComponent } from './product-item/product-item.component';
+import { ProductDetailComponent } from './product-detail/product-detail.component';
+import { ProductService } from "app/product.service";
+import { SearchComponent } from './search/search.component';
+import { StarsComponent } from './stars/stars.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    CarouselComponent,
+    FooterComponent,
+    NavbarComponent,
+    ProductItemComponent,
+    ProductDetailComponent,
+    SearchComponent,
+    StarsComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    RouterModule.forRoot([
+      {path: "", component: HomeComponent},
+      {path: "products/:productId", component: ProductDetailComponent}
+    ])
   ],
-  providers: [],
+  providers: [ProductService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
